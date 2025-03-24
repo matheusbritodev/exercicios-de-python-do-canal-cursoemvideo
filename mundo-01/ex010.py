16d9dbfddb3b671922508cab5d98c79144cf30b6
Matheus Brito <matheusbritodev1@gmail.com>
2025-03-24 02:12:55 -0300

    update

--- mundo-01/ex010.py
+++ mundo-01/ex010.py
@@ -5,4 +5,4 @@ cash = float(input('Quanto dinheiro você tem na carteira?'))
 convert = cash / 5.73
 print('Com R${} você pode comprar U${:.2f}'.format(cash, convert))
 
-#Cotação do dólar no dia 24/03/3025 --> R$5.73
+#Cotação do dólar no dia 24/03/2025 --> R$5.73
