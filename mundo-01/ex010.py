53bfcc14a4c32df780d213b4e18c48f13def77b6
Matheus Brito <matheusbritodev1@gmail.com>
2025-03-24 02:12:13 -0300

    update

---
+++ mundo-01/ex010.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+#==========================================================
+
+cash = float(input('Quanto dinheiro você tem na carteira?'))
+convert = cash / 5.73
+print('Com R${} você pode comprar U${:.2f}'.format(cash, convert))
+
+#Cotação do dólar no dia 24/03/3025 --> R$5.73
