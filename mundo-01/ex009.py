be576744e5db587f2457a7c6a0fdb702adc35c30
Matheus Brito <matheusbritodev1@gmail.com>
2025-03-24 01:58:22 -0300

    update

--- mundo-01/ex009.py
+++ mundo-01/ex009.py
@@ -2,15 +2,15 @@
 #==========================================================
 
 num = int(input('Digite um número inteiro: '))
-print('----------------',
-      '\n{} x 1 = '.format(num),num * 1,
-      '\n{} x 2 = '.format(num), num * 2,
-      '\n{} x 3 = '.format(num), num * 3,
-      '\n{} x 4 = '.format(num), num * 4,
-      '\n{} x 5 = '.format(num), num * 5,
-      '\n{} x 6 = '.format(num), num * 6,
-      '\n{} x 7 = '.format(num), num * 7,
-      '\n{} x 8 = '.format(num), num * 8,
-      '\n{} x 9 = '.format(num), num * 9,
-      '\n{} x 10 = '.format(num), num * 10,
-      '\n----------------')
+print('-' * 17,
+      '\n{} x {:2} = '.format(num, 1),num * 1,
+      '\n{} x {:2} = '.format(num, 2), num * 2,
+      '\n{} x {:2} = '.format(num, 3), num * 3,
+      '\n{} x {:2} = '.format(num, 4), num * 4,
+      '\n{} x {:2} = '.format(num, 5), num * 5,
+      '\n{} x {:2} = '.format(num, 6), num * 6,
+      '\n{} x {:2} = '.format(num, 7), num * 7,
+      '\n{} x {:2} = '.format(num, 8), num * 8,
+      '\n{} x {:2} = '.format(num, 9), num * 9,
+      '\n{} x {:2} = '.format(num, 10), num * 10)
+print('-' * 17)
