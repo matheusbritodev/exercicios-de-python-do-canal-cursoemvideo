aac393f4026c05b3a181ad877993af7263afcbed
Matheus Brito <matheusbritodev1@gmail.com>
2025-03-24 01:21:24 -0300

    update

---
+++ mundo-01/ex009.py
@@ -0,0 +1,16 @@
+#Faça um programa que leia um número inteiro qualquer e mostre na tela sua tabuada
+#==========================================================
+
+num = int(input('Digite um número inteiro: '))
+print('----------------',
+      '\n{} x 1 = '.format(num),num * 1,
+      '\n{} x 2 = '.format(num), num * 2,
+      '\n{} x 3 = '.format(num), num * 3,
+      '\n{} x 4 = '.format(num), num * 4,
+      '\n{} x 5 = '.format(num), num * 5,
+      '\n{} x 6 = '.format(num), num * 6,
+      '\n{} x 7 = '.format(num), num * 7,
+      '\n{} x 8 = '.format(num), num * 8,
+      '\n{} x 9 = '.format(num), num * 9,
+      '\n{} x 10 = '.format(num), num * 10,
+      '\n----------------')
