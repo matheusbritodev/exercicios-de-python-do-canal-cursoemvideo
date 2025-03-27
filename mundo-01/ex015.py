a0836d0b3db6d7673fe9f9a37f5ea724b1958e06
Matheus Brito <matheusbritodev1@gmail.com>
2025-03-27 13:18:26 -0300

    update

--- mundo-01/ex015.py
+++ mundo-01/ex015.py
@@ -1,4 +1,3 @@
 #Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ por dia e 0,15 por Km rodado.
 #==========================================================
 
- 
