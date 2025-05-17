05f2833cec740a9e9ce2ce20ee75806e5f94a988
Matheus Brito <matheusbritodev1@gmail.com>
2025-05-17 00:56:44 +0000

    update

---
+++ mundo-01/ex023.py
@@ -0,0 +1,5 @@
+"""Crie um programa que leia o nome completo de uma pessoa e mostre:
+O nome com todas as letras maiúsculas e minúsculas.
+Quantas letras ao todo sem considerar os espaços.
+Quantas letras tem o primeiro nome."""
+#==========================================================
